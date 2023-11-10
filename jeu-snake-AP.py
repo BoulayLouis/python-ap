8059a17c96fef57b1ae8c8b892685b18f5cde6b3
Louis Boulay <louis.boulay@etu.minesparis.psl.eu>
2023-11-10 09:32:17 +0100

    avec score

--- jeu-snake-AP.py
+++ jeu-snake-AP.py
@@ -13,14 +13,15 @@ case=20
 frame=5
 col=[5,6,7]
 ligne=[10,10,10]
-serpent=[]
+serpent=[]    #On initialise le serpent
 serpent.append((col[0]*case,ligne[0]*case))
 serpent.append((col[1]*case,ligne[1]*case))
 serpent.append((col[2]*case,ligne[2]*case))
-direction=(1,0)
-nourriture1=(3*case,3*case)
+direction=(1,0) #On initialise la direction qu'il va suivre
+nourriture1=(3*case,3*case)     #on crée les deux positions possibles pour la nourriture
 nourriture2=(15*case,10*case)
-n=1
+n=1         #on indique la nourriture avec laquelle on va commencer
+score=0
 
 
 while True:
@@ -31,40 +32,43 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
+            if event.key == pygame.K_q:  # on quitte le programme si la touvche Q est préssée
                 pygame.QUIT()
                 pygame.quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and direction != (0, 1):
+                if event.key == pygame.K_UP and direction != (0, 1):  #On change la direction si la touche UP est préssée et qu'on ne va pas déjà vers le bas (pour éviter les demi-tours)
                     direction = (0, -1)
-                elif event.key == pygame.K_DOWN and direction != (0, -1):
+                elif event.key == pygame.K_DOWN and direction != (0, -1):   #De même pour la touche DOWN
                     direction = (0, 1)
-                elif event.key == pygame.K_LEFT and direction != (1, 0):
+                elif event.key == pygame.K_LEFT and direction != (1, 0):    #De même pour la touche LEFT
                     direction = (-1, 0)
-                elif event.key == pygame.K_RIGHT and direction != (-1, 0):
+                elif event.key == pygame.K_RIGHT and direction != (-1, 0):  #De même pour la touche RIGHT
                     direction = (1, 0)
-    tete_x, tete_y = serpent[0]
+    tete_x, tete_y = serpent[0]         #on crée la nouvelle tête quand le serpent se déplace
     nouvelle_tete = (tete_x + direction[0] * case, tete_y + direction[1] * case)
-    serpent.insert(0, nouvelle_tete)
-    if serpent[0]!=nourriture1 and n==1:
-        serpent.pop()
-    elif serpent[0]!=nourriture2 and n==2:
-        serpent.pop()
-    elif serpent[0]==nourriture2 and n==2:
-        n=1
-    elif serpent[0]==nourriture1 and n==1:
-        n=2
-    screen.fill( (255, 255, 255) ) 
-    for k in range(0,largeur,2*case):
+    serpent.insert(0, nouvelle_tete)    
+    if serpent[0]!=nourriture1 and n==1:    #Si la tête du serpent ne se trouve pas sur la nourriture 1 et qu'elle est affichée
+        serpent.pop()                       #Le serpent reste de même longueur
+    elif serpent[0]!=nourriture2 and n==2:  #Si la tête du serpent ne se trouve pas sur la nourriture 2 et qu'elle est affichée
+        serpent.pop()                       #Le serpent reste de même longueur
+    elif serpent[0]==nourriture2 and n==2:  #Si la tête du serpent se trouve sur la nourriture 2 et qu'elle est affichée
+        n=1                                 #Le serpent s'allonge d'une case et on change de nourriture à afficher
+        score+=1                            #On augmente le score de 1
+    elif serpent[0]==nourriture1 and n==1:  #Si la tête du serpent se trouve sur la nourriture 1 et qu'elle est affichée
+        n=2                                 #Le serpent s'allonge d'une case et on change de nourriture à afficher
+        score+=1                            #On augmente le score de 1
+    screen.fill( (255, 255, 255) )          #On affiche un écran blanc
+    for k in range(0,largeur,2*case):       #On va afficher le cadrillage noir
         for i in range (0,hauteur,2*case):
             rect = pygame.Rect(k,i, case, case)
             rect2 = pygame.Rect(k+case,i+case, case, case)
             pygame.draw.rect(screen, noir, rect)
             pygame.draw.rect(screen, noir, rect2)
-    for position in serpent:
+    for position in serpent:                   #on affiche le serpent
         pygame.draw.rect(screen, vert, (position[0], position[1], case, case))
-    if n==1:
+    if n==1:                                    #Si on doit afficher la nourriture 1, on l'affiche
         pygame.draw.rect(screen, rouge, (nourriture1[0], nourriture1[1], case, case))
-    else:
+    else:                                       #Sinon on doit afficher la nourriture 2, on l'affiche
         pygame.draw.rect(screen, rouge, (nourriture2[0], nourriture2[1], case, case))
+    pygame.display.set_caption(f"Snake - Score : {score}")  #On affiche le score
     pygame.display.update()
