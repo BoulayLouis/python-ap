03572b15385a6f1f443f4e719a6e972596c52a9c
BoulayLouis <144105046+BoulayLouis@users.noreply.github.com>
2023-12-12 16:56:48 +0100

    lire les séquences pour avoir les couples (#21)

--- ADN.py
+++ ADN.py
@@ -1,7 +1,24 @@
 import sys
+import re
 
+
+seqid=False
+varid=False
+seq=[]
+var=[]
 for line in sys.stdin:
-    if 'Exit'==line.rstrip():
-        break
-    print(f'Processing Message from sys.stdin *****{line}*****')
-print("Done")
+    if re.match("[A,T,C,G]+",line) ==None:
+        if seqid==True and varid==True:
+            seqid,varid=False,False
+    else:
+        if varid==False:
+            if seqid==False:
+                seq.append(line.strip())
+                seqid=True
+            else:
+                var.append(line.strip())
+                varid=True
+        else:
+            var[-1]=var[-1]+line.strip()
+print(seq,var)
+
